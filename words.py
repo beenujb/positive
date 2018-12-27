6b4739e2043eda972da2a473491a6da8d53ecd5e
beenujb <41043473+beenujb@users.noreply.github.com>
2018-12-27 19:51:12 +0530

    Update words.py

--- words.py
+++ words.py
@@ -1,10 +1,9 @@
 def convert_to_words(num): 
       
-    # Get number of digits 
-    # in given number 
+   
     l = len(num);  
   
-    # Base cases  
+  
     if (l == 0): 
         print("empty string"); 
         return; 
@@ -26,57 +25,48 @@ def convert_to_words(num):
                   "sixteen", "seventeen", "eighteen", 
                   "nineteen"]; 
   
-    # The first two string are not used, 
-    # they are to make array indexing simple 
     tens_multiple = ["", "", "twenty", "thirty", "forty", 
                      "fifty", "sixty", "seventy", "eighty",  
                      "ninety"]; 
   
     tens_power = ["hundred", "thousand"]; 
   
-    # Used for debugging purpose only  
+    
     print(num, ":", end = " "); 
   
-    # For single digit number  
+    
     if (l == 1):  
         print(single_digits[ord(num[0]) - '0']); 
         return; 
   
-    # Iterate while num is not '\0'  
+    
     x = 0; 
     while (x < len(num)):  
           
-        # Code path for first 2 digits  
+      
         if (l >= 3): 
             if (ord(num[x]) - 48 != 0): 
                 print(single_digits[ord(num[x]) - 48],  
                                            end = " "); 
                 print(tens_power[l - 3], end = " ");  
-                # here len can be 3 or 4 
-                  
+                
             l -= 1; 
-              
-        # Code path for last 2 digits 
-        else: 
-              
-            # Need to explicitly handle  
-            # 10-19. Sum of the two digits 
-            # is used as index of "two_digits" 
-            # array of strings  
+           else: 
             if (ord(num[x]) - 48 == 1):  
                 sum = (ord(num[x]) - 48 + 
                        ord(num[x]) - 48); 
                 print(two_digits[sum]); 
-                return; 
+                return;    
+              
+           
   
-            # Need to explicitely handle 20  
+              
             elif (ord(num[x]) - 48 == 2 and 
                   ord(num[x + 1]) - 48 == 0): 
                 print("twenty"); 
                 return; 
                   
-            # Rest of the two digit  
-            # numbers i.e., 21 to 99  
+           
             else: 
                 i = ord(num[x]) - 48; 
                 if(i > 0): 
@@ -88,5 +78,4 @@ def convert_to_words(num):
                     print(single_digits[ord(num[x]) - 48]); 
         x += 1; 
   
-# Driver Code 
 convert_to_words("9923"); 
