340bdde0affc2dab4cf52a4ff252aac59110a609
beenujb <41043473+beenujb@users.noreply.github.com>
2019-01-06 15:39:18 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,23 @@
+def closestNumber(n, m) : 
+   q = int(n / m) 
+    n1 = m * q    
+    if((n * m) > 0) : 
+        n2 = (m * (q + 1))  
+    else : 
+        n2 = (m * (q - 1)) 
+   if (abs(n - n1) < abs(n - n2)) : 
+        return n1
+       return n2 
+      n = 13; m = 4
+print(closestNumber(n, m))       
+n = -15; m = 6
+print(closestNumber(n, m)) 
+  n = 0; m = 8
+print(closestNumber(n, m)) 
+n = 18; m = -7
+print(closestNumber(n, m)) 
+  
+  
+
+  
+
