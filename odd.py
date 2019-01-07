72fa8776a8dfea76b1a44e6a76b85b9f7388ae0b
beenujb <41043473+beenujb@users.noreply.github.com>
2019-01-07 14:23:58 +0530

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,5 +1,5 @@
-num = int(input("Enter the number: "))
-if (num % 2) == 0:
-  print("{0} is Even".format(num))
+Num = int(input("Enter the number: "))
+if (Num % 2) == 0:
+   print("{0} is Even".format(Num))
 else:
-  print("{0} is Odd".format(num))
+   print("{0} is Odd".format(Num))
