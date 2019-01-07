5f59ce7cfee3ff992d0e5c54d5d3efa380646ea8
beenujb <41043473+beenujb@users.noreply.github.com>
2019-01-07 15:18:59 +0530

    Update exits.py

--- exits.py
+++ exits.py
@@ -1,5 +1,5 @@
 for i in test_list: 
+test_list= ('4 2 1 2 3 3')
     if(i == 4) : 
         print ("Element Exists") 
-  
-print("Checking if 4 exists in list ( using in ) : ") 
+  print("Checking if 4 exists in list ( using in ) : ")  
